9c66537da63ce770889217295dabd28d4fcd33b6
nassir <daniya_n@yahoo.com>
2021-07-19 15:03:51 -0700

    pymentform

--- spacepalacefe/src/App.js
+++ spacepalacefe/src/App.js
@@ -1,26 +1,4 @@
 
-// import { Elements} from "@stripe/react-stripe-js"
-// import PaymentForm from "./pages/account/paymentForm";
-// import {loadStripe} from '@stripe/stripe-js';
-
-// const stripePromise = loadStripe("pk_test_51JF94pHQeO4M5UcZiRVQo2v9dREeoucoliWC9PndAFb1hd8AjONxAvXsqlSwJfmmjT6Mm1Asd7ZO6NoxKlExvKu200wqiFKgBx");
-
-
-
-// function App() {
-//   return (
-//     <div className="App">
-      
-//   <Elements stripe={stripePromise}>
-    
-//   <PaymentForm/>
-//   </Elements>
-      
-//     </div>
-//   );
-// }
-
-// export default App;
 
 import { useState } from 'react';
 import './App.css';
