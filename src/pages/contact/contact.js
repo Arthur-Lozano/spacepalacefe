20eaf53d90876fbeb4195da833a9fbddacd5051c
Arthur Lozano <chillout885@gmail.com>
2021-07-27 22:26:57 -0700

    updated logos and finished the form page

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Form, Button } from 'react-bootstrap';
-import styles from '../contact/contact.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ContactBanner from '../../components/ContactBanner.js'
 
+// import styles from '../contact/contact.css';
 const ContactForm = () => {
   const [state, setState] = useState({
     name: '',
@@ -12,7 +14,6 @@ const ContactForm = () => {
   });
 
   const [result, setResult] = useState(null);
-
   const sendEmail = event => {
     event.preventDefault();
     axios
@@ -33,73 +34,71 @@ const ContactForm = () => {
         });
       });
   };
-
   const onInputChange = event => {
     const { name, value } = event.target;
-
     setState({
       ...state,
       [name]: value
     });
   };
-
   return (
     <div>
+      <ContactBanner />
       {result && (
         <p className={`${result.success ? 'success' : 'error'}`}>
           {result.message}
         </p>
       )}
-      <form onSubmit={sendEmail}>
-        <Form.Group controlId="name">
-          <Form.Label>Full Name</Form.Label>
-          <Form.Control
-            type="text"
-            name="name"
-            value={state.name}
-            placeholder="Enter your full name"
-            onChange={onInputChange}
-          />
-        </Form.Group>
-
-
-        <Form.Group controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type="text"
-            name="email"
-            value={state.email}
-            placeholder="Enter your email"
-            onChange={onInputChange}
-          />
-        </Form.Group>
-        <Form.Group controlId="subject">
-          <Form.Label>Subject</Form.Label>
-          <Form.Control
-            type="text"
-            name="subject"
-            value={state.subject}
-            placeholder="Enter subject"
-            onChange={onInputChange}
-          />
-        </Form.Group>
-        <Form.Group controlId="subject">
-          <Form.Label>Message</Form.Label>
-          <Form.Control
-            as="textarea"
-            name="message"
-            value={state.message}
-            rows="3"
-            placeholder="Enter your message"
-            onChange={onInputChange}
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </form>
+      <div className={'container'}>
+        <form className={'inputform'} onSubmit={sendEmail}>
+          <Form.Group className="mb-3" controlId="name">
+            <Form.Label>Full Name</Form.Label>
+            <Form.Control
+              type="text"
+              name="name"
+              value={state.name}
+              placeholder="Enter your full name"
+              onChange={onInputChange}
+            />
+            <Form.Text className='text-muted'>We will keep your email secure</Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="text"
+              name="email"
+              value={state.email}
+              placeholder="Enter your email"
+              onChange={onInputChange}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="subject">
+            <Form.Label>Subject</Form.Label>
+            <Form.Control
+              type="text"
+              name="subject"
+              value={state.subject}
+              placeholder="Enter subject"
+              onChange={onInputChange}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="subject">
+            <Form.Label>Message</Form.Label>
+            <Form.Control
+              as="textarea"
+              name="message"
+              value={state.message}
+              rows="3"
+              placeholder="Enter your message"
+              onChange={onInputChange}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </form>
+      </div>
     </div>
   );
 };
-
 export default ContactForm;
