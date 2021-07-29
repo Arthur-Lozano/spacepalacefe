82eebb93f3fcb78f83ace282c60ba6fc246906a2
Arthur Lozano <chillout885@gmail.com>
2021-07-28 18:59:47 -0700

    updated with cards and material

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -111,7 +111,6 @@ const Product = (props) => {
                       </CardActions>
                     </Box>
                     {/* </Card> */}
-
                   </Typography>
                 </>
               )}
